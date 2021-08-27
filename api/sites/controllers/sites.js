107891b6d09f6d49c6f18ad90d85cfeff7132f21
Eli Nathan <32842898+Eli-Nathan@users.noreply.github.com>
2021-08-27 23:08:48 +0100

    Strip unwanted data from site

--- api/sites/controllers/sites.js
+++ api/sites/controllers/sites.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  findOneSafe: async (ctx) => {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services.sites.findOne({ id });
+    const sanitized = sanitizeEntity(entity, {
+      model: strapi.models.sites,
+      includeFields: ["title", "description", "Tel", "pricerange"],
+    });
+
+    const {
+      id: _id,
+      created_at,
+      published_at,
+      updated_at,
+      ...rest
+    } = sanitized;
+    return rest;
+  },
+};
