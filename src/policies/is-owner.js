65b85ae85e751b05f96bead8fc846eec2848a158
Eli Nathan <32842898+Eli-Nathan@users.noreply.github.com>
2022-01-31 22:22:46 +0000

    Refactor and comment

--- src/policies/is-owner.js
+++ src/policies/is-owner.js
@@ -1,30 +1,39 @@
 module.exports = async (policyContext, config, { strapi }) => {
   if (policyContext.state.user && policyContext.state.route) {
+    // Get api name
     const apiName = policyContext.state.route.info.apiName;
-    const [controllerName, method] =
-      policyContext.state.route.handler.split(".");
+    // Get controllerName
+    const controllerName = policyContext.state.route.handler.split(".")[0];
+
+    // apiName and controllerName name can now be used with string concatenation like api::article.article (see below)
 
     const { id } = policyContext.params;
 
+    // ID query if search is a findOne, update or delete request
+    const idQuery = { ...(id ? { id: { $eq: id } } : undefined) };
+
+    // Query db for items with owner as current user (and ID if present in request)
     const entity = await strapi.db
       .query(`api::${apiName}.${controllerName}`)
       .findMany({
         where: {
-          ...(id ? { id } : undefined),
+          ...idQuery,
           owner: { $eq: policyContext.state.user.sub },
         },
       });
 
+    // Ensure there is a filters object to use
     if (!policyContext.query) {
       policyContext.query = {};
     }
     if (!policyContext.query.filters) {
       policyContext.query.filters = {};
     }
+    //Set owner as current user
     policyContext.query.filters.owner = policyContext.state.user.sub;
 
     if (entity && entity.length > 0) {
-      return true;
+      return true; // Return true (policy allows the request through)
     }
   }
 
