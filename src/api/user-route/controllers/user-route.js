a1cb7c5ca2b9d10ebc2fc3155fc40fa95bb3d46a
Eli Nathan <32842898+Eli-Nathan@users.noreply.github.com>
2022-01-31 15:03:06 +0000

    user route routes and isOwner controllers

--- src/api/user-route/controllers/user-route.js
+++ src/api/user-route/controllers/user-route.js
@@ -1,9 +1,130 @@
-'use strict';
+"use strict";
 
 /**
  *  user-route controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
+const utils = require("@strapi/utils");
 
-module.exports = createCoreController('api::user-route.user-route');
+const { parseMultipartData } = utils;
+
+module.exports = createCoreController(
+  "api::user-route.user-route",
+  ({ strapi }) => ({
+    // find method
+    async find(ctx) {
+      let routes;
+
+      if (ctx.query) {
+        routes = await this.find({
+          ...ctx.query,
+          owner: ctx.state.user.sub,
+        });
+        return this.sanitizeOutput(routes, ctx);
+      }
+
+      routes = await this.find({
+        owner: ctx.state.user.sub,
+      });
+
+      return this.sanitizeOutput(routes, ctx);
+    },
+
+    // findOne method
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      let routes;
+
+      const [route] = await this.find({
+        id: ctx.params.id,
+        owner: ctx.state.user.sub,
+      });
+
+      if (!route) {
+        ctx.status = 404;
+        return { status: 404, message: "Route not found" };
+      }
+      return this.sanitizeOutput(route, ctx);
+    },
+
+    // findPublic method
+    async findPublic(ctx) {
+      let routes;
+
+      if (ctx.query) {
+        routes = await this.find({
+          ...ctx.query,
+          public: true,
+        });
+        return this.sanitizeOutput(routes, ctx);
+      }
+
+      routes = await this.find({
+        public: true,
+      });
+      return this.sanitizeOutput(routes, ctx);
+    },
+
+    // create method
+    async create(ctx) {
+      let entity;
+      if (ctx.is("multipart")) {
+        const { data, files } = parseMultipartData(ctx);
+        data.owner = ctx.state.user.sub;
+        entity = await this.create(data, { files });
+      } else {
+        ctx.request.body.owner = ctx.state.user.sub;
+        entity = await this.create(ctx.request.body);
+      }
+      return this.sanitizeOutput(entity, ctx);
+    },
+
+    // update method
+    async update(ctx) {
+      const { id } = ctx.params;
+
+      let entity;
+
+      const [route] = await this.find({
+        id: ctx.params.id,
+        owner: ctx.state.user.sub,
+      });
+
+      if (!route) {
+        return ctx.unauthorized(`You can't update this entry`);
+      }
+
+      if (ctx.is("multipart")) {
+        const { data, files } = parseMultipartData(ctx);
+        entity = await this.update({ id }, data, {
+          files,
+        });
+      } else {
+        entity = await this.update({ id }, ctx.request.body);
+      }
+
+      return this.sanitizeOutput(entity, ctx);
+    },
+
+    // delete method
+    async delete(ctx) {
+      const { id } = ctx.params;
+
+      let entity;
+
+      const [route] = await this.find({
+        id: ctx.params.id,
+        owner: ctx.state.user.sub,
+      });
+
+      if (!route) {
+        return ctx.unauthorized(`You can't delete this route`);
+      }
+
+      entity = await this.delete({ id });
+
+      return this.sanitizeOutput(entity, ctx);
+    },
+  })
+);
--- src/api/user-route/routes/user-route.js
+++ src/api/user-route/routes/user-route.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * user-route router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::user-route.user-route');
+module.exports = {
+  routes: [
+    {
+      method: "GET",
+      path: "/user-routes",
+      handler: "user-route.find",
+    },
+    {
+      method: "GET",
+      path: "/user-routes/public",
+      handler: "user-route.findPublic",
+    },
+    {
+      method: "GET",
+      path: "/user-routes/:id",
+      handler: "user-route.findOne",
+    },
+    {
+      method: "POST",
+      path: "/user-routes",
+      handler: "user-route.create",
+    },
+    {
+      method: "PUT",
+      path: "/user-routes/:id",
+      handler: "user-route.update",
+    },
+    {
+      method: "DELETE",
+      path: "/user-routes/:id",
+      handler: "user-route.delete",
+    },
+  ],
+};
