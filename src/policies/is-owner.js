6d2d5d3e0021d1d25d30ebab527e037911542624
Eli Nathan <32842898+Eli-Nathan@users.noreply.github.com>
2022-02-03 12:02:55 +0000

    isOwner true if owner exists

--- src/policies/is-owner.js
+++ src/policies/is-owner.js
@@ -1,40 +1,43 @@
 module.exports = async (policyContext, config, { strapi }) => {
-  if (policyContext.state.user && policyContext.state.route) {
-    // Get api name
-    const apiName = policyContext.state.route.info.apiName;
-    // Get controllerName
-    const controllerName = policyContext.state.route.handler.split(".")[0];
+  if (policyContext.state.user) {
+    if (policyContext.state.route) {
+      // Get api name
+      const apiName = policyContext.state.route.info.apiName;
+      // Get controllerName
+      const controllerName = policyContext.state.route.handler.split(".")[0];
 
-    // apiName and controllerName name can now be used with string concatenation like api::article.article (see below)
+      // apiName and controllerName name can now be used with string concatenation like api::article.article (see below)
 
-    const { id } = policyContext.params;
+      const { id } = policyContext.params;
 
-    // ID query if search is a findOne, update or delete request
-    const idQuery = { ...(id ? { id: { $eq: id } } : undefined) };
+      // ID query if search is a findOne, update or delete request
+      const idQuery = { ...(id ? { id: { $eq: id } } : undefined) };
 
-    // Query db for items with owner as current user (and ID if present in request)
-    const entity = await strapi.db
-      .query(`api::${apiName}.${controllerName}`)
-      .findMany({
-        where: {
-          ...idQuery,
-          owner: policyContext.state.user.id,
-        },
-      });
+      // Query db for items with owner as current user (and ID if present in request)
+      const entity = await strapi.db
+        .query(`api::${apiName}.${controllerName}`)
+        .findMany({
+          where: {
+            ...idQuery,
+            owner: policyContext.state.user.id,
+          },
+        });
 
-    // Ensure there is a filters object to use
-    if (!policyContext.query) {
-      policyContext.query = {};
+      // Ensure there is a filters object to use
+      if (!policyContext.query) {
+        policyContext.query = {};
+      }
+      if (!policyContext.query.filters) {
+        policyContext.query.filters = {};
+      }
+      //Set owner as current user
+      policyContext.query.filters.owner = policyContext.state.user.id;
+
+      if (entity && entity.length > 0) {
+        return true; // Return true (policy allows the request through)
+      }
     }
-    if (!policyContext.query.filters) {
-      policyContext.query.filters = {};
-    }
-    //Set owner as current user
-    policyContext.query.filters.owner = policyContext.state.user.id;
-
-    if (entity && entity.length > 0) {
-      return true; // Return true (policy allows the request through)
-    }
+    return true;
   }
 
   return false; // If you return nothing, Strapi considers you didn't want to block the request and will let it pass
--- src/policies/set-owner.js
+++ src/policies/set-owner.js
@@ -1,13 +1,15 @@
 module.exports = async (ctx, config, { strapi }) => {
-  if (ctx.state.user) {
-    if (ctx.is("multipart")) {
-      ctx.request.body.data.owner = ctx.state.user.id;
-    } else {
-      ctx.request.body.owner = ctx.state.user.id;
+  if (ctx.state.user && ctx.state.user.id) {
+    if (!ctx.request.body) {
+      ctx.request.body = {};
     }
+    if (!ctx.request.body.data) {
+      ctx.request.body.data = {};
+    }
+    ctx.request.body.data.owner = ctx.state.user.id;
 
     return true;
   }
 
-  return false; // If you return nothing, Strapi considers you didn't want to block the request and will let it pass
+  return false;
 };
