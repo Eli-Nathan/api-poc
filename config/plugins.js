fb934355c140f91fdbea8841728f0f19db774742
Eli Nathan <32842898+Eli-Nathan@users.noreply.github.com>
2022-03-07 13:29:56 +0000

    Fix cloudinary upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,17 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: process.env.CLOUDINARY_NAME,
-      api_key: process.env.CLOUDINARY_KEY,
-      api_secret: process.env.CLOUDINARY_SECRET,
+module.exports = ({ env }) => {
+  return {
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: "hnafqp5p",
+        },
+      },
     },
-    actionOptions: {
-      upload: "hnafqp5p",
-    },
-  },
-});
+  };
+};
