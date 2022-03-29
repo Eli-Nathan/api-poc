559d2b3fe1c25bcc304ce687d6046cf67519717b
Eli Nathan <32842898+Eli-Nathan@users.noreply.github.com>
2022-03-29 14:48:02 +0100

    Add quicklinks to API

---
+++ src/api/quicklink/controllers/quicklink.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  quicklink controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::quicklink.quicklink');
---
+++ src/api/quicklink/routes/quicklink.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * quicklink router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::quicklink.quicklink');
