00678f287bb149c15fe7cf8f444f95e696efa868
Eli <eli.nathan@fanduel.com>
2023-04-25 15:24:34 +0100

    Janky new stuff I need to add maybe?

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "d9b0df66ff97a666027e665707b4e3e7"),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'fd45234fab05a783ed7030497c581826'),
+    secret: env("ADMIN_JWT_SECRET", "fd45234fab05a783ed7030497c581826"),
   },
 });
